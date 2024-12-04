0e58ced1bb63a83420b15b6490b5b1e3f857c067
Hal Ng <101847895+halng@users.noreply.github.com>
2024-12-04 10:25:15 +0700

    init: centralize ci for whole project (#39)

---
+++ .github/workflows/ci.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+set -e
+
+# Detect changed folders
+detect_changed_folders() {
+    git fetch origin main
+    echo "$(git diff --name-only origin/main | awk -F'/' '{print $1}' | sort -u)"
+}
+
+install_sonar_cloud() {
+    curl -O https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-6.2.1.4610-linux-x64.zip
+    unzip *.zip
+    export PATH=$PATH:"${GITHUB_WORKSPACE}/sonar-scanner-6.2.1.4610-linux-x64/bin"
+    echo "Done: Download and Unzip Sonar Cloud"
+}
+
+install_golang_cli_lint() {
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.61.0
+}
+
+# Detect language based on folder contents
+detect_language() {
+    local folder=$1
+    if [[ -f "$folder/pom.xml" ]]; then
+        echo "java"
+        elif [[ -f "$folder/package.json" ]]; then
+        echo "js"
+        elif ls "$folder"/*.go >/dev/null 2>&1; then
+        echo "go"
+    else
+        echo "unknown"
+    fi
+}
+
+# Main CI process
+run_ci() {
+    local folder=$1
+    local language=$2
+
+    echo "========================== $folder - $language  ==========================="
+    
+    cd "$folder"
+    
+    case $language in
+        java)
+            #   ./gradlew sonarqube -Dsonar.login=$SONAR_TOKEN
+            mvn clean install
+        ;;
+        js | ts)
+            npm install
+            npm run lint
+        ;;
+        go)
+            golangci-lint run
+            go test -coverprofile coverage.out ./...
+            go test -json ./... > test-report.out
+        ;;
+        *)
+            echo "No CI steps for $language in $folder, skipping."
+        ;;
+    esac
+    
+    local project_key="anyshop_$folder"
+    sonar-scanner -Dsonar.token=$PSON_TOKEN -Dsonar.sources=. -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=tanhao111 -Dsonar.projectKey=$project_key -Dsonar.projectName=$folder
+    
+    # Build and push Docker image
+    #   if [[ -f "Dockerfile" ]]; then
+    #     docker build -t $DOCKER_USERNAME/$folder:latest .
+    #     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    #     docker push "$DOCKER_USERNAME/$folder:latest"
+    #   fi
+    
+    cd ..
+}
+
+# Run CI for each changed folder
+CHANGED_FOLDERS=$(detect_changed_folders)
+
+if [[ -z "$CHANGED_FOLDERS" ]]; then
+    echo "No relevant changes detected."
+    exit 0
+fi
+
+echo "Detected changed folders: $CHANGED_FOLDERS"
+
+echo "Starting install dependencies..."
+install_sonar_cloud
+install_golang_cli_lint
+
+echo "Starting verify components..."
+for folder in $CHANGED_FOLDERS; do
+    if [[ ! -d "$folder" ]]; then
+        echo "Folder $folder does not exist, skipping."
+        continue
+    fi
+    
+    if [[ "$folder" ==  ".github" ]]; then
+        continue
+    fi
+    
+    language=$(detect_language "$folder")
+    echo "Detected language for $folder: $language"
+    
+    if [[ "$language" == "unknown" ]]; then
+        echo "Unknown language in $folder, skipping."
+        continue
+    fi
+    
+    run_ci "$folder" "$language"
+done
