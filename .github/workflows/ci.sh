4ca6768315eae49e94e9e43a9a12992072fb9e42
halng <haonguyentan2001@gmail.com>
2024-12-07 16:43:26 +0700

    fix: typo

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -51,7 +51,7 @@ run_ci() {
     case $language in
         java)
             #   ./gradlew sonarqube -Dsonar.login=$SONAR_TOKEN
-            mvn clear
+            mvn clean
             mvn test
             mvn install
         ;;
