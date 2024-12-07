f7d2a663972e0ca27d67578a656a7897f44b038f
halng <haonguyentan2001@gmail.com>
2024-12-07 16:37:10 +0700

    feat: done test and send email

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -51,7 +51,9 @@ run_ci() {
     case $language in
         java)
             #   ./gradlew sonarqube -Dsonar.login=$SONAR_TOKEN
-            mvn clean install
+            mvn clear
+            mvn test
+            mvn install
         ;;
         js | ts)
             npm install
--- api/src/main/java/com/app/anyshop/api/config/AuthFilterConfig.java
+++ api/src/main/java/com/app/anyshop/api/config/AuthFilterConfig.java
@@ -1,6 +1,5 @@
 package com.app.anyshop.api.config;
 
-
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR;
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
@@ -36,117 +35,117 @@ import reactor.core.publisher.Mono;
 @Component
 public class AuthFilterConfig extends AbstractGatewayFilterFactory<AuthFilterConfig.Config> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AuthFilterConfig.class);
-    private static final String IAM_SERVICE = "iam";
-    private final WebClient.Builder webClientBuilder;
+  private static final Logger LOG = LoggerFactory.getLogger(AuthFilterConfig.class);
+  private static final String IAM_SERVICE = "iam";
+  private final WebClient.Builder webClientBuilder;
 
-    @Autowired private ObjectMapper objectMapper;
+  @Autowired private ObjectMapper objectMapper;
 
-    @Value("${host.iam}")
-    private String AUTH_URI;
+  @Value("${host.iam}")
+  private String AUTH_URI;
 
-    public AuthFilterConfig(WebClient.Builder webClientBuilder) {
-        super(Config.class);
-        this.webClientBuilder = webClientBuilder;
-    }
+  public AuthFilterConfig(WebClient.Builder webClientBuilder) {
+    super(Config.class);
+    this.webClientBuilder = webClientBuilder;
+  }
 
-    @Override
-    public GatewayFilter apply(Config config) {
-        String validateUrl = AUTH_URI + "/api/v1/validate";
-        return (exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-            String path = request.getURI().getPath();
-            HttpMethod method = request.getMethod();
-            LOG.info("*****************************************************************************");
-            Set<URI> uris =
-                    exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
-            String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
+  @Override
+  public GatewayFilter apply(Config config) {
+    String validateUrl = AUTH_URI + "/api/v1/validate";
+    return (exchange, chain) -> {
+      ServerHttpRequest request = exchange.getRequest();
+      String path = request.getURI().getPath();
+      HttpMethod method = request.getMethod();
+      LOG.info("*****************************************************************************");
+      Set<URI> uris =
+          exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
+      String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
 
-            //Also exclude all the incoming request to IAM service.
-            if (originalUri.contains(IAM_SERVICE)) {
-                return chain.filter(exchange);
-            }
+      // Also exclude all the incoming request to IAM service.
+      if (originalUri.contains(IAM_SERVICE)) {
+        return chain.filter(exchange);
+      }
 
-            Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
-            URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
-            if (route == null || routeUri == null) {
-                return chain.filter(exchange);
-            }
-            LOG.info(
-                    "Incoming request %s is routed to id: %s, uri: %s"
-                            .formatted(originalUri, route.getId(), routeUri));
+      Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+      URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+      if (route == null || routeUri == null) {
+        return chain.filter(exchange);
+      }
+      LOG.info(
+          "Incoming request %s is routed to id: %s, uri: %s"
+              .formatted(originalUri, route.getId(), routeUri));
 
-            // TODO: change get token from header instead of `authorization`
-            if (ExcludeUrlConfig.isSecure(path, method)) {
-                String bearerToken =
-                        Objects.requireNonNull(request.getHeaders().get("authorization")).get(0);
+      // TODO: change get token from header instead of `authorization`
+      if (ExcludeUrlConfig.isSecure(path, method)) {
+        String bearerToken =
+            Objects.requireNonNull(request.getHeaders().get("authorization")).get(0);
 
-                if (bearerToken == null) {
-                    LOG.error("Can not access to secure end point with null token");
-                    return onError(
-                            exchange,
-                            "Can not access to secure end point " + "with null token",
-                            "Token is null",
-                            HttpStatus.UNAUTHORIZED);
-                }
-                return webClientBuilder
-                        .build()
-                        .get()
-                        .uri(validateUrl)
-                        .header("Authorization", bearerToken)
-                        .retrieve()
-                        .bodyToMono(AuthValidateVm.class)
-                        .map(
-                                res -> {
-                                    exchange.getRequest().mutate().header("username", res.username());
-                                    exchange.getRequest().mutate().header("authority", res.authority());
-                                    LOG.info(
-                                            "Success validate user. Forward to %s"
-                                                    .formatted(exchange.getRequest().getPath()));
-                                    return exchange;
-                                })
-                        .flatMap(chain::filter)
-                        .onErrorResume(
-                                err -> {
-                                    LOG.error("Error when validating account");
-                                    HttpStatusCode errCode = null;
-                                    String errMsg = "";
-                                    if (err instanceof WebClientResponseException exception) {
-                                        errCode = exception.getStatusCode();
-                                        errMsg = exception.getMessage();
-                                    } else {
-                                        errCode = HttpStatus.BAD_GATEWAY;
-                                        errMsg = HttpStatus.BAD_GATEWAY.getReasonPhrase();
-                                    }
-                                    return onError(exchange, errMsg, "JWT Authentication Failed", errCode);
-                                });
-            }
-            LOG.info("*****************************************************************************");
-            return chain.filter(exchange);
-        };
-    }
-
-    private Mono<Void> onError(
-            ServerWebExchange exchange, String err, String errDetails, HttpStatusCode httpStatus) {
-        DataBufferFactory dataBufferFactory = exchange.getResponse().bufferFactory();
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(httpStatus);
-        try {
-            response.getHeaders().add("Content-Type", "application/json");
-            ResVm data = new ResVm(httpStatus, err, errDetails, null, new Date());
-            byte[] byteData = objectMapper.writeValueAsBytes(data);
-            return response.writeWith(Mono.just(byteData).map(dataBufferFactory::wrap));
-
-        } catch (JsonProcessingException e) {
-            LOG.error(e.getMessage());
+        if (bearerToken == null) {
+          LOG.error("Can not access to secure end point with null token");
+          return onError(
+              exchange,
+              "Can not access to secure end point " + "with null token",
+              "Token is null",
+              HttpStatus.UNAUTHORIZED);
         }
-        return response.setComplete();
-    }
+        return webClientBuilder
+            .build()
+            .get()
+            .uri(validateUrl)
+            .header("Authorization", bearerToken)
+            .retrieve()
+            .bodyToMono(AuthValidateVm.class)
+            .map(
+                res -> {
+                  exchange.getRequest().mutate().header("username", res.username());
+                  exchange.getRequest().mutate().header("authority", res.authority());
+                  LOG.info(
+                      "Success validate user. Forward to %s"
+                          .formatted(exchange.getRequest().getPath()));
+                  return exchange;
+                })
+            .flatMap(chain::filter)
+            .onErrorResume(
+                err -> {
+                  LOG.error("Error when validating account");
+                  HttpStatusCode errCode = null;
+                  String errMsg = "";
+                  if (err instanceof WebClientResponseException exception) {
+                    errCode = exception.getStatusCode();
+                    errMsg = exception.getMessage();
+                  } else {
+                    errCode = HttpStatus.BAD_GATEWAY;
+                    errMsg = HttpStatus.BAD_GATEWAY.getReasonPhrase();
+                  }
+                  return onError(exchange, errMsg, "JWT Authentication Failed", errCode);
+                });
+      }
+      LOG.info("*****************************************************************************");
+      return chain.filter(exchange);
+    };
+  }
 
-    public static class Config {
+  private Mono<Void> onError(
+      ServerWebExchange exchange, String err, String errDetails, HttpStatusCode httpStatus) {
+    DataBufferFactory dataBufferFactory = exchange.getResponse().bufferFactory();
+    ServerHttpResponse response = exchange.getResponse();
+    response.setStatusCode(httpStatus);
+    try {
+      response.getHeaders().add("Content-Type", "application/json");
+      ResVm data = new ResVm(httpStatus, err, errDetails, null, new Date());
+      byte[] byteData = objectMapper.writeValueAsBytes(data);
+      return response.writeWith(Mono.just(byteData).map(dataBufferFactory::wrap));
 
-        public Config() {
-            // TODO document why this constructor is empty
-        }
+    } catch (JsonProcessingException e) {
+      LOG.error(e.getMessage());
     }
+    return response.setComplete();
+  }
+
+  public static class Config {
+
+    public Config() {
+      // TODO document why this constructor is empty
+    }
+  }
 }
